91451d884e6d0fe37513ba362fd3fa2561dcd0a1
SergeyMoshkov <sergejmoshkov@gmail.com>
2022-11-12 06:14:40 +0300

    Grouping REST API Requests

--- main.go
+++ main.go
@@ -7,11 +7,23 @@ import (
 )
 
 func main() {
+
+	// HTTP GET, POST, PUT, and DELETE requests
 	r := gin.Default()
 	r.GET("/", GetHello)
 	r.POST("/", PostHello)
 	r.PUT("/", PutHello)
 	r.DELETE("/", DeleteHello)
+
+	// group requests
+	r1 := r.Group("/api") 
+	{
+        r1.GET("/hello", GetHello)
+        r1.POST("/hello", PostHello)
+        r1.PUT("/hello", PutHello)
+		r1.DELETE("/hello", DeleteHello)
+	}
+
 	r.Run(":8080")
 	fmt.Println("server listening on")
 
